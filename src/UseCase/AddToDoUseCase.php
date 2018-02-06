5701924e396201e5f1458ed868f8c7326964585e
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2018-02-06 10:02:05 +0100

    Add Business Rules for AddToDoUseCase

--- src/UseCase/AddToDoUseCase.php
+++ src/UseCase/AddToDoUseCase.php
@@ -3,21 +3,25 @@
 namespace SharktheFire\ToDo\UseCase;
 
 use SharktheFire\ToDo\ToDoRepository;
+use SharktheFire\ToDo\ToDoRequest;
+use SharktheFire\ToDo\ToDoResponse;
 
-class AddToDoUseCase {
-
+class AddToDoUseCase implements UseCase
+{
     private $repository;
 
-    public function __construct(ToDoRepository $repository) {
+    public function __construct(ToDoRepository $repository)
+    {
         $this->repository = $repository;
     }
 
-    public function execute(ToDoRequest $request) {
+    public function execute(ToDoRequest $request): ToDoResponse
+    {
         $id = $request->id;
         $content = $request->content;
 
-        $toDo = new ToDo($id, $content);
+        $toDo = $this->repository->create($id, $content);
 
-        $this->repository->addToDo($toDo);
+        return new ToDoResponse($toDo);
     }
 }
