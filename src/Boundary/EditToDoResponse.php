9e3cb6c18ebad9517ff3474487cda355ee97e557
Jennifer Eschenhorn <jennifer.eschenhorn@jimdo.com>
2018-03-01 09:50:20 +0100

    Fix EditToDoUseCase - It should only edit content if isFinished is false

--- src/Boundary/EditToDoResponse.php
+++ src/Boundary/EditToDoResponse.php
@@ -6,10 +6,36 @@ use SharktheFire\ToDo\ToDo;
 
 class EditToDoResponse
 {
-    public $toDo;
+    private $toDo;
 
-    public function __construct(ToDo $toDo)
+    private $editContentIsPossible;
+
+    public function __construct(ToDo $toDo, bool $editContentIsPossible)
     {
         $this->toDo = $toDo;
+        $this->editContentIsPossible = $editContentIsPossible;
+    }
+
+    public function getId()
+    {
+        return $this->toDo->id();
+    }
+
+    public function getContent()
+    {
+        return $this->toDo->content();
+    }
+
+    public function getStatus()
+    {
+        return $this->toDo->isFinished();
+    }
+
+    public function showCorrectText()
+    {
+        if ($this->editContentIsPossible) {
+            return "Das ToDo mit der ID " . $this->getId() . " wurde erfolgreich bearbeitet! -> " . $this->getContent();
+        }
+        return "Das ToDo mit der ID " . $this->getId() . " konnte nicht mehr bearbeitet werden! Finish Status ist -> " . ($this->getStatus() ? 'true' : 'false') . PHP_EOL;
     }
 }
